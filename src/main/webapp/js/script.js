b1a12034903ceb071e2b1522ba461c4c1d2e6a74
Kevin Gonzalez <kgagudelo@gmail.com>
2023-05-27 18:41:14 -0500

    Truco

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,112 +1,83 @@
-var charts = [];
-
-var ctxGameStop = document.getElementById('chartGameStop').getContext('2d');
-var chartGameStop = createChart(ctxGameStop, 'GameStop');
-
-var ctxApple = document.getElementById('chartApple').getContext('2d');
-var chartApple = createChart(ctxApple, 'Apple');
-
-var ctxTesla = document.getElementById('chartTesla').getContext('2d');
-var chartTesla = createChart(ctxTesla, 'Tesla');
-
-var ctxMicrosoft = document.getElementById('chartMicrosoft').getContext('2d');
-var chartMicrosoft = createChart(ctxMicrosoft, 'Microsoft');
-
-var ctxGoogle = document.getElementById('chartGoogle').getContext('2d');
-var chartGoogle = createChart(ctxGoogle, 'Google');
-
-var ctxOpera = document.getElementById('chartOpera').getContext('2d');
-var chartOpera = createChart(ctxOpera, 'Opera');
-
-var ctxDiscord = document.getElementById('chartDiscord').getContext('2d');
-var chartDiscord = createChart(ctxDiscord, 'Discord');
-
-var ctxSamsung = document.getElementById('chartSamsung').getContext('2d');
-var chartSamsung = createChart(ctxSamsung, 'Samsung');
-
-charts.push(chartGameStop, chartApple, chartTesla, chartMicrosoft, chartGoogle, chartOpera, chartDiscord, chartSamsung);
-
-setInterval(updateData, 1000);
-
-function createChart(ctx, label) {
-  var newData = [];
-  var hour = [];
-  for (var i = 1; i <= 60; i++) {
-    hour.push(i.toString());
-  }
-  for (var i = 0; i < 12; i++) {
-    newData.push(Math.floor(Math.random() * 300) + 1);
-  }
-
-  var chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: hour,
-      datasets: [{
-        label: label,
-        data: newData,
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      }
+/**
+ * 
+ */
+var url = "http://localhost:8081/api/grafica";
+var xhr = new XMLHttpRequest();
+xhr.open('get', url, true);
+xhr.onload = function() {
+    var graficas = JSON.parse(xhr.responseText);
+    if (xhr.status == "202") {
+        console.table(graficas);
+        for (var i = 0; i < graficas.length; i++) {
+            var info = graficas[i];
+            createChart(info.nombre); // Crear gráfico para cada elemento del arreglo
+        }
+    } else {
+        console.error(graficas);
     }
-  });
-
-  return chart;
 }
+xhr.send(null);
 
-function updateData() {
-  var hour = [];
-  for (var i = 1; i <= 60; i++) {
-    hour.push(i.toString());
-  }
+function createChart(nombre) {
+    var container = document.getElementById('acciones');
 
-  for (var j = 0; j < charts.length; j++) {
-    var chart = charts[j];
+    // Create a div element to hold the chart
+    var chartDiv = document.createElement('div');
+    chartDiv.classList.add('myChart');
+    container.appendChild(chartDiv);
 
-    var newData = [];
-    for (var i = 0; i < 12; i++) {
-      newData.push(Math.floor(Math.random() * 300) + 1);
-    }
-
-    var lastHourIndex = chart.data.labels.length - 1;
-    var lastHourName = chart.data.labels[lastHourIndex];
-    var nextHourIndex = hour.indexOf(lastHourName) + 1;
-    if (nextHourIndex >= hour.length) {
-      nextHourIndex = 0;
-    }
-    chart.data.labels.push(hour[nextHourIndex]);
-    chart.data.datasets[0].data.push(newData[0]);
-    chart.update();
-  }
-}
-
-var newChartForm = document.getElementById('newChartForm');
-newChartForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  var chartNameInput = document.getElementById('chartName');
-  var chartName = chartNameInput.value;
-
-  if (chartName !== '') {
+    // Create a canvas element inside the chart div
     var canvas = document.createElement('canvas');
-    var chartId = 'chart' + (charts.length + 1);
-    canvas.id = chartId;
-    document.body.insertBefore(canvas, newChartForm);
+    chartDiv.appendChild(canvas);
 
     var ctx = canvas.getContext('2d');
-    var newChart = createChart(ctx, chartName);
-    charts.push(newChart);
-  }
 
-  chartNameInput.value = '';
-});
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: generateChartData(nombre),
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            }
+        }
+    });
+
+    setInterval(function() {
+        updateData(myChart, nombre);
+    }, 5000);
+}
 
 
+function generateChartData(nombre) {
+    var data = {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60'],
+        datasets: [{
+            label: nombre,
+            data: [Math.floor(Math.random() * 300) + 1],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
+    };
+
+    return data;
+}
+
+function updateData(chart, nombre) {
+    var newData = [];
+ 
+        newData.push(Math.floor(Math.random() * 300) + 1);
+    
+    // Push the new data to the existing dataset
+    chart.data.datasets[0].data.push(...newData);
+    
+    // Limit the dataset length to 60 data points
+    if (chart.data.datasets[0].data.length > 60) {
+        chart.data.datasets[0].data.shift(); // Remove the oldest data point
+    }
+
+    chart.update();
+}

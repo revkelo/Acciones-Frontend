5bce62645112aa526ceeb9c29f8264e708bec55d
Kevin Gonzalez <kgagudelo@gmail.com>
2023-06-04 03:38:36 -0500

    Grafica

--- src/main/webapp/js/acciones.js
+++ src/main/webapp/js/acciones.js
@@ -20,6 +20,8 @@ var graficasData = [];
 
 document.getElementById('generarPdfLink').addEventListener('click', generarPdf);
 
+document.getElementById('tendenciaPdfLink').addEventListener('click', tendenciaPdf);
+
 
 function generarPdf(event) {
 	event.preventDefault();
@@ -39,6 +41,77 @@ function generarPdf(event) {
 		id = usuarioArray[0];
 		console.log(id);
 		pdf(id);
+
+	}
+	xhr.send(null);
+
+
+}
+
+function tendenciaPdf(event) {
+	event.preventDefault();
+
+
+
+	var url = "http://localhost:8081/api/tendencia";
+	var xhr = new XMLHttpRequest();
+	xhr.open('get', url, true);
+
+
+	xhr.onload = function() {
+
+		try {
+			// Code that might throw an exception
+
+			var empresa = JSON.parse(xhr.responseText);
+			if (xhr.status == 202) {
+
+
+				var tableRows = [];
+
+
+				for (var i = 0; i < empresa.length; i++) {
+					var info = empresa[i];
+
+
+					var row = [
+						info.nombreEmpresa,
+						info.promedio,
+						info.movimientos,
+
+					];
+
+
+					tableRows.push(row);
+				}
+
+				var docDefinition = {
+					content: [
+						'¡Hola, esto es un PDF generado con pdfmake!',
+						{
+							table: {
+								headerRows: 1,
+								body: [
+									['nombre', 'promedio', 'movimientos'],
+									...tableRows
+								]
+							}
+						}
+					]
+				};
+
+
+				pdfMake.createPdf(docDefinition).open();
+
+
+
+
+			} else {
+				console.error(empresa);
+			}
+		} catch (error) {
+			alert("Error");
+		}
 
 	}
 	xhr.send(null);
@@ -276,7 +349,7 @@ function generateChartData(nombre, precioAccion) {
 		labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60'],
 		datasets: [{
 			label: nombre,
-			data: [Math.floor(Math.random() * 300) + 1],
+			data: [precioAccion],
 			borderColor: 'rgba(255, 99, 132, 1)',
 			borderWidth: 1
 		}]
@@ -289,28 +362,39 @@ function updateData(chart, priceSpan, nombre) {
 	var newData = [];
 	var lastData = chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1];
 	var randomValue = Math.floor(Math.random() * 2) === 0 ? -1 : 1;
-
 	newData.push(lastData + randomValue);
-
-
 	chart.data.datasets[0].data.push(...newData);
-
 
 	if (chart.data.datasets[0].data.length > 60) {
 		chart.data.datasets[0].data.shift();
 	}
 
-
 	var newPrice = parseFloat(priceSpan.textContent.split(':')[1].trim()) + randomValue;
 	priceSpan.textContent = 'Precio de acción: ' + newPrice;
 
+	// Save the current price to the database using XMLHttpRequest
+	var url = 'http://localhost:8081/api/tendencia';
+	var xhr = new XMLHttpRequest();
+	var params = 'nombreEmpresa=' + encodeURIComponent(nombre) + '&valor=' + encodeURIComponent(newPrice);
 
-	for (var i = 0; i < graficasData.length; i++) {
-		if (graficasData[i].nombre === nombre) {
-			graficasData[i].precioAccion = newPrice;
-			break;
+	xhr.open('POST', url + '?' + params, true);
+
+	xhr.onload = function() {
+		try {
+			var success = xhr.responseText.toLowerCase() === 'true';
+			if (xhr.status == '200') {
+
+				console.log('Creado');
+			} else {
+				console.error('Error');
+
+			}
+		} catch (err) {
+			console.log('Error: ' + err);
 		}
-	}
+	};
+
+	xhr.send(null);
 
 	chart.update();
 }
